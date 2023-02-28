8623994898fad132de9d102d67841f9272c41f25
Hayden Chen <brooklyn1984@qq.com>
2023-02-28 19:52:40 +0800

    feat: ✨ initial commit

---
+++ src/components/Draw.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { Excalidraw } from '@excalidraw/excalidraw'
+
+const Draw: React.FC<{}> = () => {
+  return (
+    <>
+      <p>Draw</p>
+      <Excalidraw />
+    </>
+  )
+}
+
+export default Draw
